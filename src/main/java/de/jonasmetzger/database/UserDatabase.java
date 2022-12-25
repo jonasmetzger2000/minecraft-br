fea07978f09666f8a7c475fefbade5799f38c7c1
Jonas <mail@jonasmetzger.de>
2022-12-26 00:37:18 +0100

    start basic user system.

--- src/main/java/de/jonasmetzger/database/UserDatabase.java
+++ src/main/java/de/jonasmetzger/database/UserDatabase.java
@@ -1,13 +1,37 @@
 package de.jonasmetzger.database;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import de.jonasmetzger.dependency.Inject;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.UUID;
 
 public class UserDatabase {
 
     @Inject
     FileConfiguration config;
 
+    @Inject
+    MongoDatabase mongoDatabase;
+
+    @RequiredArgsConstructor
+    static class User {
+        private final UUID id;
+        private final Role role;
+
+        enum Role {
+            NORMAL,
+            VIP,
+            MVP,
+            PRO,
+            MEDIA,
+            MODERATOR_TRIAL,
+            MODERATOR,
+            ADMIN
+        }
+    }
 
 
 }
